90ffa022458164438d8e9c3b1a72ce1857cd416f
samotwix <36455283+twixonater@users.noreply.github.com>
2018-03-19 09:00:59 -0400

    HTML and CSS
    Start to final project

---
+++ js/design.js
@@ -0,0 +1,8 @@
+//Side Nav Bar JS
+document.getElementsById('homenav').onmouseover= underLineNav; 
+
+
+
+function underLineNav() {
+	document.getElementsById('homenav').style.borderColor= black;
+}
