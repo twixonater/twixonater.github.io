cb9c6cbf2771be5c5c6484065de3ab159553e466
samotwix <36455283+twixonater@users.noreply.github.com>
2018-03-22 18:34:07 -0400

    compile css and first stab at js

--- js/design.js
+++ js/design.js
@@ -1,8 +1,21 @@
-//Side Nav Bar JS
-document.getElementsById('homenav').onmouseover= underLineNav; 
+// Home navigation to side navigation
+//When a user clicks on a nav item from the "home", the side nav will stack.
+var topNav=true;
+
+$(".topnavitem").on("click", sideNav);
+
+function sideNav(){
+	if (topNav) {
+		$(".topnavitem").removeClass("topnavitem").addClass("sidenavitem");
+		$(".logolanding").removeClass("logolanding").addClass("logoheader");
+		topNav=false;
+		} 
+	else {
+		$(".sidenavitem").removeClass("sidenavitem").addClass("topnavitem");
+		$(".logoheader").removeClass("logoheader").addClass("logolanding");
+		topNav=true;
+		}
+};
 
 
 
-function underLineNav() {
-	document.getElementsById('homenav').style.borderColor= black;
-}
