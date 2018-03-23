da7644733418bd8a040158816a00b04d638b8ada
samotwix <36455283+twixonater@users.noreply.github.com>
2018-03-23 08:17:01 -0400

    removed links to other pages

--- js/design.js
+++ js/design.js
@@ -1,21 +1,18 @@
 // Home navigation to side navigation
-//When a user clicks on a nav item from the "home", the side nav will stack.
-var topNav=true;
-
+//When a user clicks on a nav item from the "home", the side nav will stack and move beneath the logo
 $(".topnavitem").on("click", sideNav);
 
 function sideNav(){
-	if (topNav) {
-		$(".topnavitem").removeClass("topnavitem").addClass("sidenavitem");
-		$(".logolanding").removeClass("logolanding").addClass("logoheader");
-		topNav=false;
-		} 
-	else {
-		$(".sidenavitem").removeClass("sidenavitem").addClass("topnavitem");
-		$(".logoheader").removeClass("logoheader").addClass("logolanding");
-		topNav=true;
-		}
+		$(".topnavitem").hide(1200);
+		$(".logolanding").hide(1200);
+		$("aside").slideDown(1200);
+		$(".logoheader").slideDown(1200);	
 };
 
+//Home mode will always have the topnav 
+$(".shomenav").on("click", function(){
+	$(".topnavitem").slideDown(1200);
+	$(".logolanding").slideDown(1200);
+});
 
 
