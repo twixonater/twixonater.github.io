9d21c4527083505fd677306e32866923ba7af05e
samotwix <36455283+twixonater@users.noreply.github.com>
2018-04-19 13:14:35 -0400

    Updated styles
    -tablet styles
    -animation for nav appearing
    -gallery fix
    -style adjustments

--- js/design.js
+++ js/design.js
@@ -1,42 +1,59 @@
-//On scrolling, the text will move on hero section
+
 $(window).scroll(function(){
-	if ($(window).width() <= 480) {
-	    if($(window).scrollTop() > 600) {
-	        //CELLPHONE ANIMATE HERE
-
-	    }
-	} else if ($(window).width() > 480 && $(window).width() <= 1024) {
-	    if($(window).scrollTop() > 480){
-	        // TABLET ANIMATION HERE
-
-	    }
-	} else {
-	    if ($(window).scrollTop() > 10) {
-	        // DESKTOP ANIMATION HERE
-	        $(".work").slideDown(1200, function(){
-				$(".skill").fadeIn(1200, function(){
-					$(".art").fadeIn(1200, function(){
-						$(".resume").slideDown(1200, function(){
-							$(".contact").fadeIn(1200, function(){
-							})
+	var intro = $(".contact").offset().top;
+	var workTitle = $(".workhistory").offset().top;
+	var skillsTitle = $(".skillset").offset().top;
+	var artTitle = $(".artgallery").offset().top;
+	var bioTitle = $(".resumedownload").offset().top;
+	var contactTitle = $(".contactform").offset().top;
+	$(".work").animate({opacity: 1},1200, function(){
+				$(".skill").animate({opacity: 1},1200, function(){
+					$(".art").animate({opacity: 1},1200, function(){
+						$(".resume").animate({opacity: 1},1200, function(){
+							$(".contact").animate({opacity: 1},1200							
+							)
 						})
 					})
 				})			
-			}) 	
-		} 
-} 
+			})
+	if ($(window).scrollTop()> intro && $(window).scrollTop() < workTitle && $(window).width() > 1024) {
+		$("nav").css({"display":"flex", "justify-content":"space-around"});
+
+	} else if ($(window).scrollTop() > workTitle && $(window).scrollTop() < skillsTitle){
+		$(".largeWork").animate({
+					opacity: 1
+				}, 1000);
+
+	} else if ($(window).scrollTop() > skillsTitle - 300 && $(window).scrollTop() < artTitle){ 
+		$(".largeSkills").animate({
+					opacity: 1
+				}, 1000);
+
+	} else if ($(window).scrollTop() > artTitle - 100 && $(window).scrollTop() < bioTitle){ 
+		$(".largeArt").animate({
+					opacity: 1
+				}, 1000);
+
+	} else if ($(window).scrollTop() > bioTitle - 100 && $(window).scrollTop() < contactTitle){ 
+		$(".largeBio").animate({
+					opacity: 1
+				}, 1000);	
+	} else if ($(window).scrollTop() >= contactTitle - 100) { 
+		$(".largeContact").animate({
+					opacity: 1
+				}, 1000);
+
+	}
 });
 
-
-
-
-
-
-
-
-
-
-
+//If a .navitems is clicked prior to activating the scroll of the nav coming down, activate Nav in new section
+$(".navitems").on("click", function(){
+	if ($(window).width() > 1024){
+	$("nav").css({"display":"flex","justify-content":"space-around"});
+	} else {
+		$("nav").css({"display":"none"});
+	}
+})
 
 
 // on clicking of the hamburger, show the nav by animating it's left value to 0
@@ -56,67 +73,164 @@ $(".close-nav").on("click", function(){
 });
 
 
+// When a user clicks on an image, it enlarges with the caption text; get me the image in artImage that corresponds with the same index number.
+	$(".tiles").on("click", function() {
+		$(".modal").css({"display":"block"});
+		var i;
+		var imageNo = $(this).index();
+		$(".artImage img").eq(imageNo).addClass("active");
+		$(".captiontext p").eq(imageNo).addClass("active");
+	  	$(".thumbguide img").eq(imageNo).addClass("active");
+
+	});
+
+	$(".close").on("click", function(){
+		$(".modal").css({"display":"none"})
+		$(".active").removeClass("active")
+	})
+
 // When you click on a gallery item, it enlarges in a lightbox
 	var totalImages = $("img").length;
+	var lastImage = ($("img").length -1);
 	var currentImageIndex;
 	$(".prev").on("click", function(){
-	currentImageIndex = $(".active").prev().index();
-	if (currentImageIndex >= 0 && currentImageIndex < totalImages) {
-	   $(".active").removeClass("active").prev().addClass("active");
+		currentImageIndex = $(".active").prev().index();
+		if (currentImageIndex > 0 && currentImageIndex < totalImages) {
+	   	$(".active").removeClass("active").prev().addClass("active");
+	  } else if (currentImageIndex <= 0) {
+	  	console.log(currentImageIndex);
+	  	currentImageIndex = lastImage;
+	  	$(".active").removeClass("active");
+	  	$(".artImage img").last().addClass("active");
+	  	$(".captiontext p").last().addClass("active")
+	  	$(".thumbguide img").last().addClass("active");
 	  }
-	}); $(".next").on("click", function(){
+	});
+
+
+	 $(".next").on("click", function(){
 		  	currentImageIndex = $(".active").next().index();
-		  if (currentImageIndex >= 0 && currentImageIndex < totalImages) {
+		  if (currentImageIndex >= 0 && currentImageIndex <= lastImage) {
 		    $(".active").removeClass("active").next().addClass("active");
+		  } else if (lastImage === totalImages - 1) { 
+		  	currentImageIndex = 0;
+			$(".active").removeClass("active");
+		  	$(".artImage img").first().addClass("active");
+		  	$(".captiontext p").first().addClass("active");
+		  	$(".thumbguide img").first().addClass("active");
 		  }
 		});
 
-
-//When you hover over .navitems, they will display the help text to what they contain
- 	$(".worknav").hover(
+//When you hover over .navitem, they will display the help text to what they contain
+ 	$(".work").hover(
 	 	function() {
-	 		$(".work .navitems").css({"opacity":"0.5"});
-	 		$(".work .navitems").text("Where I've Worked");	
+	 		$(".work .navitem").css({"opacity":"0.5"});
+	 		$(".work .navitem").text("Where I've Worked");	
 	 	}, function () {
-	 		$(".work .navitems").css({"opacity":"1"});
-	 		$(".work .navitems").text("Work");
+	 		$(".work .navitem").css({"opacity":"1"});
+	 		$(".work .navitem").text("Work");
 	 	});
 
- 	$(".skillsnav").hover(
+ 	$(".skill").hover(
 	 	function() {
-	 		$(".skills .navitems").css({"opacity":"0.5"});
-	 		$(".skills .navitems").text("What I can do");	
+	 		$(".skill .navitem").css({"opacity":"0.5"});
+	 		$(".skill .navitem").text("My Skills Are");	
 	 	}, function (){
-	 		$(".skills .navitems").css({"opacity":"1"});
-			$(".skills .navitems").text("Skills");
+	 		$(".skill .navitem").css({"opacity":"1"});
+			$(".skill .navitem").text("Skills");
 	 	});
 
- 	$(".artnav").hover(
+ 	$(".art").hover(
  		function(){
- 			$(".art .navitems").css({"opacity":"0.5"});
- 			$(".art .navitems").text("Gallery");	
+ 			$(".art .navitem").css({"opacity":"0.5"});
+ 			$(".art .navitem").text("Gallery");	
 	 	}, function(){
-	 		$(".art .navitems").css({"opacity":"1"});
-			$(".art .navitems").text("Art")
+	 		$(".art .navitem").css({"opacity":"1"});
+			$(".art .navitem").text("Fine Art")
 	 	});
 
- 	$(".resumenav").hover(
+ 	$(".resume").hover(
 	 	function() {
-	 		$(".resume .navitems").css({"opacity":"0.5"});
-	 		$(".resume .navitems").text("Download resume");	
+	 		$(".resume .navitem").css({"opacity":"0.5"});
+	 		$(".resume .navitem").text("Résumé");	
 	 	}, function(){
-	 		$(".resume .navitems").css({"opacity":"1"});
-			$(".resume .navitems").text("Resume")
+	 		$(".resume .navitem").css({"opacity":"1"});
+			$(".resume .navitem").text("Bio")
 	 	});	
 
- 	$(".contactnav").hover(
+ 	$(".contact").hover(
  		function(){
-	 		$(".contact .navitems").css({"opacity":"0.5"});
-	 		$(".contact .navitems").text("Let's Collaborate");	
+	 		$(".contact .navitem").css({"opacity":"0.5"});
+	 		$(".contact .navitem").text("Email");	
  		}, function (){
- 			$(".contact .navitems").css({"opacity":"1"});
-			$(".contact .navitems").text("Contact")
+ 			$(".contact .navitem").css({"opacity":"1"});
+			$(".contact .navitem").text("Contact")
  		});
+
+// When you click on a .navitem, it will take you to that section ,and the title will animate
+
+	$(".worknav").on("click", function(){
+		resetTitles();
+		$(".largeWork").animate({opacity: 1}, 1000);
+	});
+
+	$(".skillsnav").on("click", function(){
+		resetTitles();
+		$(".largeSkills").animate({opacity: 1}, 1000);
+	});
+
+	$(".artnav").on("click", function(){
+		resetTitles();
+		$(".largeArt").animate({opacity: 1}, 1000);
+	});
+
+	$(".resumenav").on("click", function(){
+		resetTitles();
+		$(".largeBio").animate({opacity: 1}, 1000);
+	});
+
+	$(".contactnav").on("click", function(){
+		resetTitles();
+		$(".largeContact").animate({opacity: 1}, 1000);
+	});
+
+
+function resetTitles(){
+	$(".titleLarge").css({"opacity":"0"});
+};	
+
+// When you click on a .navitems, it will take you to that section ,and the title will animate
+
+	$(".work").on("click", function(){
+		resetTitles();
+		$(".largeWork").animate({opacity: 1}, 1000);
+	});
+
+	$(".skill").on("click", function(){
+		resetTitles();
+		$(".largeSkills").animate({opacity: 1}, 1000);
+	});
+
+	$(".art").on("click", function(){
+		resetTitles();
+		$(".largeArt").animate({opacity: 1}, 1000);
+	});
+
+	$(".resume").on("click", function(){
+		resetTitles();
+		$(".largeBio").animate({opacity: 1}, 1000);
+	});
+
+	$(".contact").on("click", function(){
+		resetTitles();
+		$(".largeContact").animate({opacity: 1}, 1000);
+	});
+
+
+function resetTitles(){
+	$(".titleLarge").css({"opacity":"0"});
+};	
+
 
 // When you Click on the Download REsume, Download Resume takes you to a new window
 $(".downloadRes").on("click", function(){
@@ -124,8 +238,3 @@ $(".downloadRes").on("click", function(){
 	});
 
 
-//When you click on Samo Davis, it will take you to the top of the page
-
-$(".logo").on("click", function(){
-	window.location.href = "index.html";
-});
---
+++ js/google-sheet.js
@@ -0,0 +1,66 @@
+// Variable to hold request
+var request;
+
+// Bind to the submit event of our form
+$("#samocontactform").submit(function(event){
+
+    // Abort any pending request
+    if (request) {
+        request.abort();
+    }
+    // setup some local variables
+    var $form = $(this);
+
+    // Let's select and cache all the fields
+    var $inputs = $form.find("input, select, button, textarea");
+
+    // Serialize the data in the form
+    var serializedData = $form.serialize();
+
+    // Let's disable the inputs for the duration of the Ajax request.
+    // Note: we disable elements AFTER the form data has been serialized.
+    // Disabled form elements will not be serialized.
+    $inputs.prop("disabled", true);
+
+    // Fire off the request to /form.php
+    request = $.ajax({
+        url: "https://script.google.com/macros/s/AKfycbw6bAnG37j1Vnjg8n1p0afoO0YkvfmEXFzVs8AiFf0-h7jHAfQe/exec",
+        type: "post",
+        data: serializedData
+    });
+
+    // Callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR){
+        // Log a message to the console
+        console.log("Hooray, it worked!");
+        console.log(response);
+        console.log(textStatus);
+        console.log(jqXHR);
+    });
+
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // Log the error to the console
+        console.error(
+            "The following error occurred: "+
+            textStatus, errorThrown
+        );
+    });
+
+    // Callback handler that will be called regardless
+    // if the request failed or succeeded
+    request.always(function () {
+        // Reenable the inputs
+        $inputs.prop("disabled", false);
+    });
+
+    // Prevent default posting of form
+    event.preventDefault();
+});
+
+// When the submit button is clicked, the Thank you screen appears
+$(".submitemail").on("click", function(){
+    $("#samocontactform").css("opacity","0");
+    $(".contactMe").text("Thank You");
+    $(".letsCollaborate p").html("Thanks for the message!<br><br>Looking forward to collaborating with you.<br><br>I will get back to you shortly");
+})
